48fcfc5dea7d76342524a608caf319e76f5a644e
Paul Battley <pbattley@gmail.com>
2008-05-31 23:10:55 +0000

    Make the errors describe themselves. This will facilitate reuse across CLI/GUI interfaces. L10n isn't a high priority for this project, anyway, for the simple reason that it's only usable in the UK.

--- lib/iplayer/errors.rb
+++ lib/iplayer/errors.rb
@@ -1,8 +1,31 @@
 module IPlayer
 module Errors
-  ParsingError    = Class.new(RuntimeError)
-  FileUnavailable = Class.new(RuntimeError)
-  MP4Unavailable  = Class.new(RuntimeError)
-  MetadataError   = Class.new(RuntimeError)
+  class ParsingError < RuntimeError
+    def to_s
+      "Unable to parse the programme page. Possible reasons:\n"+
+      "1. The iPlayer has changed\n"+ 
+      "2. You are outside the UK (and not using a UK proxy).\n"+
+      "3. The BBC think that you are outside the UK."
+    end
+  end
+
+  class FileUnavailable < RuntimeError
+    def to_s
+      "An MP4 URL was found, but the download failed."
+    end
+  end
+  
+  class MP4Unavailable < RuntimeError
+    def to_s
+      "This programme is not currently available in an MP4 version."
+    end
+  end
+
+  class MetadataError < RuntimeError
+    def to_s
+      "Unable to parse the metadata for this programme.\n"+
+      "As a workaround, you can use the -f option to specify a filename manually."
+    end
+  end
 end
 end
