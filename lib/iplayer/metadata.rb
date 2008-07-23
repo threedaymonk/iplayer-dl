6fc16caaa7f5b79e4a310dc92d505004591ff449
Paul Battley <pbattley@gmail.com>
2008-07-23 20:23:27 +0000

    Programme title for creating subdirectories wasn't determined correctly.

--- lib/iplayer/metadata.rb
+++ lib/iplayer/metadata.rb
@@ -12,18 +12,24 @@ class Metadata
     @browser = browser
   end
 
+  def title
+    mixed_title.split(/:/).first
+  end
+
+  def full_title
+    mixed_title.gsub(/\s*:\s*/, ' - ')
+  end
+
+private
+
   def metadata
     @metadata ||= REXML::Document.new( @browser.get(METADATA_URL % @pid).body )
   rescue Exception => e
     raise MetadataError, e.message
   end
 
-  def title
+  def mixed_title
     REXML::XPath.first(metadata, '//playlist/title').text
-  end
-
-  def full_title
-    title.gsub(/\s*:\s*/, ' - ')
   end
 
 end
